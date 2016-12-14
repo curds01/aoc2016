0720d6fb911744f52b8e001a839d67ffc7cda1b1
Sean Curtis <Sean Curtis>
2016-12-13 21:45:07 -0800

    Slight code refactoring
    Look up of precomputation now belongs *inside* the hex computation.

--- day14/day14.py
+++ day14/day14.py
@@ -9,35 +9,30 @@ from heapq import *
 import md5
 
 TRIPLE_RE = re.compile('.*?((?P<A>.)(?P=A)(?P=A))')
+salts = {}
 
 def getHex( hash, count ):
-    hex = hash
-    for i in xrange( count ):
-        hex = md5.md5( hex ).hexdigest()
-    return hex
+    global salts
+    if ( salts.has_key( hash ) ):
+        return salts[ hash ]
+    else:
+        hex = hash
+        for i in xrange( count ):
+            hex = md5.md5( hex ).hexdigest()
+        salts[ hash ] = hex
+        return hex
 
 def generateKeys( salt, stretchCount, targetCount ):
     i = 0
     count = 0
-    salts = {}
     while ( count < targetCount ):
-        hash = salt + '%d' % i
-        if ( salts.has_key( hash ) ):
-            hex = salts[ hash ]
-        else:
-            hex = getHex( hash, stretchCount + 1 )
-            salts[hash] = hex
+        hex = getHex( salt + '%d' % i, stretchCount + 1 )
         m = TRIPLE_RE.match( hex )
         if ( m ):
             re_str = '.*?%s{5}' % m.group(2)
             five_re = re.compile(re_str )
             for j in xrange( i + 1, i + 1 + 1000 ):
-                hash = salt + '%d' % j
-                if ( salts.has_key( hash ) ):
-                    hex = salts[ hash ]
-                else:
-                    hex = getHex( salt + '%d' % j, stretchCount + 1 )
-                    salts[ hash ] = hex
+                hex = getHex( salt + '%d' % j, stretchCount + 1 )
                 m = five_re.match( hex )
                 if ( m ):
                     count += 1
